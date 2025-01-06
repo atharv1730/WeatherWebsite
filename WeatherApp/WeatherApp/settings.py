035ffee86a1835db0fbc87e9921b3f8152dfb966
atharv1730 <atharvkumar06@gmail.com>
2025-01-06 11:39:53 -0500

    Template SetUp

--- WeatherApp/WeatherApp/settings.py
+++ WeatherApp/WeatherApp/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'WeatherApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'weather/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello from my view!")
-
+  url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=27101254d3f497eed5b1d80a07b3fcd1'
+  return render(request, 'weather/index.html')
