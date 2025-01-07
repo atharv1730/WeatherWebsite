3c4704bd377b35f2b934b9c79ba5b15f6f82b2ac
atharv1730 <atharvkumar06@gmail.com>
2025-01-07 00:58:27 -0500

    Getting Weather Data

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -4,13 +4,18 @@ import requests
 # Create your views here.
 
 def index(request):
-    url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid=d18fc568438a833cbcd4fdeb36def429"
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=d18fc568438a833cbcd4fdeb36def429"
 
     city = "Las Vegas"
+    r = requests.get(url.format(city)).json()
 
-    r = requests.get(url.format(city))
+    city_weather = {
+        "city": city,
+        "temperature": r['main']['temp'],
+        "description": r['weather'][0]['description'],
+        "icon": r['weather'][0]['icon'],
+    }
 
-    print(r.text)
 
     return render(request, 'weather/index.html')
 
