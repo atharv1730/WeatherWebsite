9fb8141de0dfec3fbb85e9a794c9fab2e0d5b32a
atharv1730 <atharvkumar06@gmail.com>
2025-01-07 01:28:37 -0500

    Final

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,22 +1,32 @@
 from django.shortcuts import render
 import requests
 
-# Create your views here.
-
 def index(request):
+    # OpenWeatherMap API URL
     url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=d18fc568438a833cbcd4fdeb36def429"
 
-    city = "Las Vegas"
-    r = requests.get(url.format(city)).json()
+    city = "Las Vegas"  # Default city
+    city_weather = {}  # Initialize city_weather dictionary
 
-    city_weather = {
-        "city": city,
-        "temperature": r['main']['temp'],
-        "description": r['weather'][0]['description'],
-        "icon": r['weather'][0]['icon'],
-    }
+    if request.method == "POST":
+        city = request.POST.get("city", "Las Vegas")  # Get city from user input
 
+    try:
+        # Make API request
+        r = requests.get(url.format(city))
+        r.raise_for_status()  # Raise HTTPError for bad responses
+        data = r.json()
 
-    return render(request, 'weather/index.html')
+        # Extract weather details
+        city_weather = {
+            "city": city,
+            "temperature": data['main']['temp'],
+            "description": data['weather'][0]['description'],
+            "icon": data['weather'][0]['icon'],
+        }
+    except requests.exceptions.RequestException:
+        city_weather = {"error": "Unable to retrieve data. Please enter a valid city."}
 
+    # Render template with weather data
+    return render(request, 'weather/index.html', {"city_weather": city_weather})
 
